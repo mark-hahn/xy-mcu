5a4b1caedc451b725a7bd844dd45495d8e869a6d
Mark Hahn <mark@hahnca.com>
2017-03-25 21:40:47 -0700

    added accelsFromSpiWord

--- parse-spi.c
+++ parse-spi.c
@@ -3,7 +3,7 @@
 #include "parse-spi.h"
 
 // return number of leading ones in a 32-bit word
-char numLeading1s(uint32_t *word) {
+uint8_t numLeading1s(uint32_t *word) {
   char byt, mask = 0x80;
   if((byt = ((char *) word)[3]) != 0xff) {
     for(char count = 0; ; count++, mask >> 1) 
@@ -27,80 +27,83 @@ char numLeading1s(uint32_t *word) {
 // returns axis from spi vector (X:0, Y:1)
 char axisFromSpiWord(uint32_t *word) {
   switch (numLeading1s(word)) {
-    case  1: return (*word & 0x10000000) != 0;
-    case  7: return (*word & 0x00800000) != 0;
-    case  3: return (*word & 0x08000000) != 0;
-    case  6: return (*word & 0x01000000) != 0;
-    case  2: return (*word & 0x10000000) != 0;
-    case  5: return (*word & 0x01000000) != 0;
-    case  4: return (*word & 0x02000000) != 0;
-    case 10: return (*word & 0x00100000) != 0;
-    case  9: return (*word & 0x00200000) != 0;
-    case 14: return (*word & 0x00010000) != 0;
-    case 26: return (*word & 0x00000010) != 0;
+    case  1: return ((((char *) word)[3]) & 0x10) != 0;
+    case  7: return ((((char *) word)[2]) & 0x80) != 0;
+    case  3: return ((((char *) word)[3]) & 0x08) != 0;
+    case  6: return ((((char *) word)[3]) & 0x01) != 0;
+    case  2: return ((((char *) word)[3]) & 0x10) != 0;
+    case  5: return ((((char *) word)[3]) & 0x01) != 0;
+    case  4: return ((((char *) word)[3]) & 0x02) != 0;
+    case 10: return ((((char *) word)[2]) & 0x10) != 0;
+    case  9: return ((((char *) word)[2]) & 0x20) != 0;
+    case 14: return ((((char *) word)[2]) & 0x01) != 0;
+    case 26: return ((((char *) word)[0]) & 0x10) != 0;
   }
+  return 0;  // shouldn't get here
 }
 
 // returns accelerations from packed spi word
 // first array item is number of packed values (len)
-// second item is offset (ofs), e.g. accel=2, ofs=4, actual accel is -2
-void accelsFromSpiWord(uint32_t word, uint8_t *accels) {
-  uint32_t mask;
-  char i, len, ofs, bits, mask;
+// each item value is sign-extended to int8_t
+void accelsFromSpiWord(uint32_t *word, int8_t *accels) {
+  uint32_t wordVal = *word;
+  uint8_t  i, len, bits, mask, byteVal, signMask, extbits;
   switch (numLeading1s(word)) {
     case  3: 
       len  = 9;
       bits = 3;
-      ofs  = 4;
       mask  = 0x07;
       break;
     case  6: 
       len  = 8;
       bits = 3;
-      ofs  = 4;
       mask  = 0x07;
       break;
     case  2: 
       len  = 7;
       bits = 4;
-      ofs  = 8;
       mask  = 0x0f;
       break;
     case  5: 
       len  = 6;
       bits = 4;
-      ofs  = 8;
       mask  = 0x0f;
       break;
     case  4: 
       len  = 5;
       bits = 5;
-      ofs  = 16;
       mask  = 0x1f;
       break;
     case 10: 
       len  = 4;
       bits = 5;
-      ofs  = 16;
       mask  = 0x1f;
-       break;
+      break;
     case  9: 
       len  = 3;
       bits = 7;
-      ofs  = 64;
       mask  = 0x7f;
       break;
     case 14: 
       len  = 2;
-      bits = 8;
-      ofs  = 128;
-      mask  = 0xff;
+//    bits = 8;
+//    mask  = 0xff;
       break;
   }
   accels[0] = len;  
-  accels[1] = ofs;
-  for(i=len+1; i > 1; i--, word >>= bits)
-    accels[i] = word & mask;
+  if(len != 2) {
+    signMask = (mask+1) >> 1;
+    extbits  = ~mask;
+    for(i=len; i > 0; i--, wordVal >>= bits) {
+      byteVal = wordVal & mask;
+      if(byteVal & signMask) byteVal |= extbits;
+      accels[i] = (int8_t) byteVal;
+    }
+  }
+  else {
+    accels[1] = ((int8_t *) word)[1];
+    accels[2] = ((int8_t *) word)[0];
+  }
 }
 
 
--- parse-spi.h
+++ parse-spi.h
@@ -1,17 +1,11 @@
-/* 
- * File:   parse-spi.h
- * Author: root
- *
- * Created on March 25, 2017, 4:24 PM
- */
 
 #ifndef PARSE_SPI_H
 #define	PARSE_SPI_H
 
 #include <stdint.h>
 
-char numLeading1s(uint32_t *word);
-char axisFromSpiWord(uint16_t *word);
+uint8_t axisFromSpiWord(uint32_t *word);
+void    accelsFromSpiWord(uint32_t *word, int8_t *accels);
 
 #endif	/* PARSE_SPI_H */
 
